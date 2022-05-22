0636fd8a8793e74ad11b826f738539c7dcf7fc37
jmane <45289031+xlordj@users.noreply.github.com>
2022-05-21 22:00:22 -0400

    firts commit

---
+++ src/test/java/pack/NewTest.java
@@ -0,0 +1,30 @@
+package pack;
+import org.testng.annotations.Test;
+
+import com.xpandit.testng.annotations.Xray;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.*;		
+		
+
+public class NewTest {		
+    private WebDriver driver;		
+	@Test
+	@Xray (requirement = "HTUTO-34", test="HTUTO-35")
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		AssertJUnit.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+	    driver = new ChromeDriver();  
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}		
+}	
